cc88723245e3b5c1e917c2be39fb17713ba4c1f3
Srinath-N-Gudi <srinathngudigit11@gmail.com>
2025-07-08 21:02:06 +0530

    Added function callback for every render call in Renderer

--- Renderer/GL/Renderer.cpp
+++ Renderer/GL/Renderer.cpp
@@ -8,9 +8,17 @@ namespace Nyx {
                 : m_VAOs(vaoList), m_Count(vaoCount), m_DrawMode(drawMode) {
             }
 
-            void Renderer::draw() {
+            void Renderer::draw(DrawCallback callback, void* userData) {
                 for (size_t i = 0; i < m_Count; ++i) {
                     VAO* vao = m_VAOs[i];
+
+                    bool skipDraw = false;
+                    if (callback) {
+                        callback(static_cast<int>(i), vao, m_UserData, skipDraw);
+                    }
+
+                    if (skipDraw) continue;
+
                     vao->bind();
                     if (vao->hasIBO()) {
                         glDrawElements(m_DrawMode, vao->getIBO()->getCount(), GL_UNSIGNED_INT, nullptr);
--- Renderer/GL/Renderer.h
+++ Renderer/GL/Renderer.h
@@ -8,6 +8,8 @@
 #error "No OpenGL loader defined. Define NYX_USE_GLAD or NYX_USE_GLEW before including Nyx headers."
 #endif
 
+#include <functional> // for std::function
+
 #include "VAO.h"
 #include "IBO.h"
 
@@ -17,14 +19,19 @@ namespace Nyx {
 
             class Renderer {
             public:
+                using DrawCallback = std::function<void(int index, VAO* vao, void* userData, bool& skipDraw)>;
+
                 Renderer(VAO** vaoList, size_t vaoCount, GLenum drawMode);
 
-                void draw();
+                void draw(DrawCallback callback = nullptr, void* userData = nullptr);
 
             private:
                 VAO** m_VAOs;
                 size_t m_Count; // number of VAOs
                 GLenum m_DrawMode;
+
+                DrawCallback m_Callback;
+                void* m_UserData;
             };
 
         } // namespace GL
